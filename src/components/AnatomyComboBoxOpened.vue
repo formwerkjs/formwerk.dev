7c7aa5c6469a2ccb7df29d8d1edf8d491ebceb88
Abdelrahman Awad <logaretm1@gmail.com>
2025-01-18 14:24:13 +0200

    feat: combobox anatomy

---
+++ src/components/AnatomyComboBoxOpened.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex w-full items-start justify-center gap-10">
+    <div class="flex w-full max-w-xs flex-col">
+      <span>Label</span>
+
+      <div
+        class="flex w-full items-center justify-center rounded-md border border-zinc-300 bg-zinc-400 px-4 py-1.5 font-medium text-zinc-700 dark:border-zinc-600 dark:bg-zinc-900 dark:text-zinc-400"
+      >
+        <div class="inline-flex w-min items-center">
+          Search
+          <span
+            class="ml-0.5 inline-block h-6 w-px animate-pulse bg-zinc-400"
+          ></span>
+        </div>
+        <PhCaretDown class="ml-auto" />
+      </div>
+
+      <AnatomyPart
+        text="ListBox"
+        position="bottom"
+        :distance="20"
+        class="w-full"
+      >
+        <div
+          class="mt-1 flex w-full flex-col gap-1 rounded-md border border-zinc-300 bg-zinc-400 px-4 py-2 dark:border-zinc-600 dark:bg-zinc-900"
+        >
+          <div>Option 1</div>
+          <AnatomyPart text="Option item" position="right" :distance="20">
+            <div>Option 2</div>
+          </AnatomyPart>
+          <div>Option 3</div>
+        </div>
+      </AnatomyPart>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AnatomyPart from './AnatomyPart.vue';
+import PhCaretDown from '~icons/ph/caret-down';
+</script>
