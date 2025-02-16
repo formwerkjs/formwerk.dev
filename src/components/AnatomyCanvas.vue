d8ff2762d2e5ab8e6b74a0173053914603c2afb5
Abdelrahman Awad <logaretm1@gmail.com>
2025-02-16 23:45:21 +0200

    feat: document calendar composable

--- src/components/AnatomyCanvas.vue
+++ src/components/AnatomyCanvas.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="not-content flex select-none flex-col items-center justify-center gap-28 rounded-lg bg-zinc-200 px-4 py-20 dark:bg-zinc-800"
+    class="not-content relative flex select-none flex-col items-center justify-center gap-28 rounded-lg bg-zinc-200 px-4 py-20 dark:bg-zinc-800"
   >
     <slot />
   </div>
